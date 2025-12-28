9fee3bf2635a61c7305a54517cdfe1623b3582f5
siddharth-oz <krishforsiddhu@gmail.com>
2025-12-28 13:13:10 +0530

    intial commit

--- main.sql
+++ main.sql
@@ -23,4 +23,19 @@ SELECT department_id,SUM(salary)
 FROM DEPARTMENT
 GROUP BY department_id;
 
-SELECT department_id AS "Department_id"
+SELECT department_id AS "Department Code",
+COUNT(*) AS "No of Employees
+SUM(salary) AS "Total Salary"
+FROM DEPARTMENT
+GROUP BY department_id;
+
+SELECT department_id AS "Department Code",
+SUM(salary) AS "Total Salary"
+FROM DEPARTMENT
+WHERE MANAGER ID= '103'
+GROUP BY department_id;
+
+SELECT department_id, COUNT(*) AS "No. of Employees"
+FROM DEPARTMENT
+GROUP BY department_id
+HAVING COUNT(*) > 2;
